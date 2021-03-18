c700af7418c724e0236dd6b89648ea5a7dbc48d3
ssijann <rockstarsijan@gmail.com>
2021-03-18 17:36:09 +0545

    homework

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -4,39 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.twoactivities.R;
+import com.example.twoactivities.SecondActivity;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_TEXT = "me..threebuttonstwoactivities.extra.TEXT";
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_COUNT = "helloToastHW1.COUNT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
     }
 
-    public void showText(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-
-//        insert some random text; got from randomtextgenerator.com
-        switch (view.getId()){
-            case R.id.button_one:
-                intent.putExtra(EXTRA_TEXT, getString(R.string.display_one)); startActivity(intent);
-                break;
-            case R.id.button_two:
-                intent.putExtra(EXTRA_TEXT, getString(R.string.display_two)); startActivity(intent);
-                break;
-            case R.id.button_three:
-                intent.putExtra(EXTRA_TEXT, getString(R.string.display_three)); startActivity(intent);
-                break;
-            default:
-                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_LONG).show();
-                break;
+    public void countUp(View view) {
+        mCount ++;
+        if (mShowCount != null){
+            mShowCount.setText(String.format(Locale.ENGLISH, "%d", mCount));
         }
+    }
+
+
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_COUNT, mCount);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,21 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
-    private TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        displayText = findViewById(R.id.display_text);
         Intent intent = getIntent();
-        String display = intent.getStringExtra(MainActivity.EXTRA_TEXT);
-        displayText.setText(display);
+        int mCount = intent.getIntExtra(MainActivity.EXTRA_COUNT, 0);
+        TextView mCountTextView = findViewById(R.id.count_text);
+        mCountTextView.setText(String.valueOf(mCount));
     }
 }
